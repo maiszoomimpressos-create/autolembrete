f37b4dafebc1951dedaf862826adcabe44c264fa
[dyad] <maiszoomimpressos@gmail.com>
2025-11-13 22:21:36 -0300

    [dyad] Added 'use client' to all new page components to fix JSX parsing errors - wrote 4 file(s)

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React from 'react';
 import MainHeader from '@/components/MainHeader';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
