dd47516da6be5945996e8da53f6ff03b19b138ef
[dyad] <maiszoomimpressos@gmail.com>
2025-11-13 22:48:49 -0300

    [dyad] wrote 1 file(s)

--- src/pages/HistoryPage.tsx
+++ src/pages/HistoryPage.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import React from 'react';
 import MainHeader from '@/components/MainHeader';
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
